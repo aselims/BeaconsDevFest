15b9a7429815656ad0cf54a244502bb12a05cf9f
aselims <selim.2k@gmail.com>
2015-11-12 20:44:15 +0100

    button and scan

--- app/src/main/java/co/rahala/beaconsdevfest/MainActivity.java
+++ app/src/main/java/co/rahala/beaconsdevfest/MainActivity.java
@@ -1,16 +1,22 @@
 package co.rahala.beaconsdevfest;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private BluetoothManager bluetoothManager;
     private BluetoothAdapter bluetoothAdapter;
+    private TextView stopScanTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,27 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
 
+        final BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
+            @Override
+            public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+                Log.i(TAG, "Address: " + bluetoothDevice.getAddress() +
+                                "\n RSSI:" + i +
+                                "\n name:" + bluetoothDevice.getName());
+            }
+        };
 
+        bluetoothAdapter.startLeScan(scanCallback);
+
+        findViewById(R.id.btn_stop_scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bluetoothAdapter.stopLeScan(scanCallback);
+            }
+        });
 
 
     }
